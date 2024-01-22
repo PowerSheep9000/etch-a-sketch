48a6a976e106bdd61e12331d9200b3c37d7cf3ef
Lukas Kokerle <lukas.kokerle@gmx.at>
2024-01-22 07:49:33 +0100

    Add basic creation of squares

---
+++ etch.js
@@ -0,0 +1,9 @@
+drawSquares();
+
+function drawSquares() {
+    for (let i = 0; i < 16; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        document.querySelector(".container").appendChild(square);
+    }
+}
