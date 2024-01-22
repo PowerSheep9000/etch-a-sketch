8b6be6c544dc98e3e5f6b3d0f23e2abfe70e9e3f
Lukas Kokerle <lukas.kokerle@gmx.at>
2024-01-22 08:55:36 +0100

    Add functionality to choose number of squares + color change

--- etch.js
+++ etch.js
@@ -1,9 +1,38 @@
-drawSquares();
+const btn = document.querySelector("#promptButton");
 
-function drawSquares() {
-    for (let i = 0; i < 16; i++) {
+let numberOfSquares = btn.addEventListener("click", () => {
+    drawSquares(prompt("Enter number of squares:"));
+    const squareList = getNumberOfSquares();
+    squareList.forEach(changeColor);
+    squareList.forEach(resetColor);
+});
+
+function drawSquares(numberOfSquares) {
+    if (numberOfSquares > 100) {
+        numberOfSquares = 100;
+    }
+
+    for (let i = 0; i < numberOfSquares; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
         document.querySelector(".container").appendChild(square);
     }
+}
+
+function changeColor(square) {
+    square.addEventListener("mouseover", () => {
+        //square.classList.add("hovered");
+        square.setAttribute("style", "background-color: teal");
+    });
+}
+
+function resetColor(square) {
+    square.addEventListener("mouseout", () => {
+        //square.classList.remove("hovered");
+        square.setAttribute("style", "background-color: white");
+    });
+}
+
+function getNumberOfSquares() {
+    return document.querySelectorAll(".square");
 }
