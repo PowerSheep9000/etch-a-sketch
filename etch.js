71a28705c32849cfda404a7ed7bd028c4b8c801e
Lukas Kokerle <lukas.kokerle@gmx.at>
2024-01-22 13:24:06 +0100

    Rework grid construction + logic + add changing colors

--- etch.js
+++ etch.js
@@ -1,38 +1,55 @@
 const btn = document.querySelector("#promptButton");
+let numberOfSquares;
 
-let numberOfSquares = btn.addEventListener("click", () => {
-    drawSquares(prompt("Enter number of squares:"));
-    const squareList = getNumberOfSquares();
-    squareList.forEach(changeColor);
-    squareList.forEach(resetColor);
+btn.addEventListener("click", () => {
+    numberOfSquares = prompt("Enter number of squares:");
+    clearScreen();
+    createSquares(numberOfSquares);
+    changeColor();
+    resetColor();
 });
 
-function drawSquares(numberOfSquares) {
+function createSquares(numberOfSquares) {
     if (numberOfSquares > 100) {
         numberOfSquares = 100;
     }
 
-    for (let i = 0; i < numberOfSquares; i++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        document.querySelector(".container").appendChild(square);
+    for (let i = 0; i <= numberOfSquares; i++) {
+        for (let j = 0; j <= numberOfSquares; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.style.height = (800 / numberOfSquares) + "px";
+            square.style.width = (800 / numberOfSquares) + "px";
+            document.querySelector(".container").appendChild(square);
+        }
     }
 }
 
-function changeColor(square) {
-    square.addEventListener("mouseover", () => {
-        //square.classList.add("hovered");
-        square.setAttribute("style", "background-color: teal");
+function clearScreen() {
+    const screen = document.querySelector(".container");
+
+    while (screen.firstChild) {
+        screen.removeChild(screen.lastChild);
+    }
+}
+
+function changeColor() {
+    let squares = document.querySelectorAll("div .square");
+    squares.forEach(function(elem) {
+        let colorR = Math.floor(Math.random() * (255 + 1));
+        let colorG = Math.floor(Math.random() * (255 + 1));
+        let colorB = Math.floor(Math.random() * (255 + 1));
+        elem.addEventListener("mouseover", function() {
+            elem.style.backgroundColor = `rgb(${colorR}, ${colorG}, ${colorB})`;
+        });
     });
 }
 
-function resetColor(square) {
-    square.addEventListener("mouseout", () => {
-        //square.classList.remove("hovered");
-        square.setAttribute("style", "background-color: white");
+function resetColor() {
+    let squares = document.querySelectorAll("div .square");
+    squares.forEach(function(elem) {
+        elem.addEventListener("mouseout", function() {
+            elem.style.backgroundColor = "";
+        });
     });
-}
-
-function getNumberOfSquares() {
-    return document.querySelectorAll(".square");
 }
